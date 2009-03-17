7ea4aed5a9a0c22890c788fe8e0a37dc5d3f58be
mkaganer <mkaganer@315d4af9-c016-9649-a7cf-b8f2ab84a462>
2009-03-17 14:25:42 +0000

    B.H. sql package rewrite, now all connection config is passed as associative array to sql_connection::open()
    Also added support for mysql extention (previously only mysqli was supported)
    
    TODO: filters and schema support
    
    git-svn-id: http://svn.mehadrin-serv.com/svn/pkgman/trunk@10 315d4af9-c016-9649-a7cf-b8f2ab84a462

--- core/sql/config.php
+++ core/sql/config.php
@@ -1,30 +1,40 @@
 <?php
 // B.H.
 
-// Package config file (optional):
+/**
+ * @desc Package config file (optional)
+ * Predefined variables: $pkg_name, $path, $prefix
+ */
 
-// Predefined variables:
-// $pkg_name, $path, $prefix
-
-// Default config for the package
+/**
+ * @desc Package descriptor - Default config for the package
+ * @var array $config
+ */
 $config = array(
+
   // default charset to be used by the connection
   'charset' => 'utf-8',
-  
+
+  // If true,  throw sql_exception's on SQL errors
+  // otherwise query will return false
+  'throw_on_error' => true,
+
   'drivers' => array(
     'mysqli' => 'sql_mysqli_connection',
+    'mysql' => 'sql_mysql_connection'
   ),
 
 );
 
-// * Optional: package control class (must inherit pkgman_package)
-// Example:
-// class <name> extends pkgman_package {
-// ....
-// }
-// $package = new <name>($pkg_name,$prefix,$path,$config)
-//
-// * (if $package is not defined, pkgman_manager will automatically
-// * create an instance of a generic pkgman_package class)
- 
+/*
+     * Optional: package control class (must inherit pkgman_package)
+     Example:
+     class <name> extends pkgman_package {
+     ....
+     }
+     $package = new <name>($pkg_name,$prefix,$path,$config)
+
+     * (if $package is not defined, pkgman_manager will automatically
+     * create an instance of a generic pkgman_package class)
+*/
 ?>
---
+++ core/sql/mysql_result.class.php
@@ -0,0 +1,41 @@
+<?php
+// B.H.
+
+class sql_mysql_result extends sql_result {
+
+  /**
+   * @var sql_mysql_connection
+   */
+  private $connection;
+
+  /**
+   * @var mysql result resource
+   */
+  private $result;
+
+  // should be called by sql_mysql_connection::query
+  public function __construct($connection,$result) {
+    if (empty($result)) throw new Exception("Empty result from a query");
+    $this->connection = $connection;
+    $this->result = $result;
+  }
+
+  public function __destruct() {
+  	mysql_free_result($this->result);
+  }
+
+  function num_rows() {
+  	return mysql_num_rows($this->result);
+  }
+
+  function row(&$data) {
+  	$data = mysql_fetch_array($this->result,MYSQL_NUM);
+    return !(empty($data));
+  }
+  function arow(&$data) {
+    $data = mysql_fetch_array($this->result,MYSQL_ASSOC);
+    return !(empty($data));
+  }
+
+}
+?>
