f325fb6dd1a6e3b2ad33fa7b8d84d6fbf0e3f045
mkaganer <mkaganer@315d4af9-c016-9649-a7cf-b8f2ab84a462>
2009-03-17 19:12:36 +0000

    Added phpdocs to the pkgman core classes
    git-svn-id: http://svn.mehadrin-serv.com/svn/pkgman/trunk@12 315d4af9-c016-9649-a7cf-b8f2ab84a462

--- core/pkgman_package.class.php
+++ core/pkgman_package.class.php
@@ -1,12 +1,39 @@
 <?php
 // B.H.
 
+/**
+ * @desc Base class for package descriptors. A package may define a custom descriptor class which
+ * must extend this class. In that case, you have to assign a class descriptor to $package varible
+ * in config.php in package's directory.
+ * @author mkaganer
+ */
 class pkgman_package {
 
-  public $name; // package name (like "html/form")
-  public $prefix; // classes prefix (like "html_form_")
-  public $path; // path to the package
-  
+  /**
+   * @desc Package name (like "html/form")
+   * @var string
+   */
+  public $name;
+
+
+  /**
+   * @desc classes prefix (like "html_form_")
+   * @var string
+   */
+  public $prefix;
+
+
+  /**
+   * @desc A file system path to the package
+   * @var string
+   */
+  public $path;
+
+  /**
+   * @desc Package's config, merged from default config in config.php and values passed when the class
+   * is imported
+   * @var array
+   */
   public $config;
 
   public function __construct($pkg_name,$prefix,$path,$config=null) {
@@ -16,16 +43,24 @@ class pkgman_package {
     $this->config = $config;
     if (!is_array($this->config)) $this->config = array();
   }
-  
-  // simple implementation of merge_config, but i hope it is sufficient
+
+  /**
+   * @desc Modify package's config using values from $conf.
+   * This is very simple implementation of merge_config, but i hope it is sufficient
+   * @param string $conf
+   */
   public function merge_config($conf) {
     if (!is_array($conf)) return;
     $this->config = array_merge_recursive($this->config,$conf);
   }
-  
-  // called by the autoload engine to load a specific class
-  // must return true on success, or false if class is not found
-  // if $test_only == true, then do not parse the class's file, only check if it is present
+
+  /**
+   * @desc Called by the autoload engine to load a specific class must return true on success,
+   * or false if class is not found
+   * @param string $class_name - a class to load
+   * @param bool $test_only - if true, do not actually load class, only check if it is present
+   * @return bool
+   */
   public function load_class($class_name,$test_only=false) {
     if (strcspn($class_name,"#=\"\\?*:/@|<>.\$") != strlen($class_name))
       die("Invalid class name: [$class_name]");
@@ -37,7 +72,7 @@ class pkgman_package {
     if (!file_exists($file)) return false;
     if (!$test_only) include($file);
     return true;
-  } 
+  }
 
 }
 
