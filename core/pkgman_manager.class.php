65e44f31f3a220e54540a654c74792ef599e2ee5
mkaganer <mkaganer@315d4af9-c016-9649-a7cf-b8f2ab84a462>
2009-03-17 19:12:36 +0000

    Added phpdocs to the pkgman core classes
    git-svn-id: https://svn.mehadrin-serv.com/svn/pkgman/trunk@12 315d4af9-c016-9649-a7cf-b8f2ab84a462

--- core/pkgman_manager.class.php
+++ core/pkgman_manager.class.php
@@ -1,37 +1,42 @@
 <?php
 // B.H.
 
+/**
+ * @desc Base class for the package engine implementation. Maintains a list of package "roots" locations
+ * where the system will look for a package directories to import.
+ * @author mkaganer
+ */
 class pkgman_manager {
 
   public $roots; // array of root paths for packages library
 
   private $imported; // array of ("pkg" => pkgman_package) for all imported packages
-  
+
   // enforce single instance
   private static $instance = null;
-  
+
   public static function get_instance() {
     if (!empty(self::$instance)) return self::$instance;
     return self::$instance = new self();
   }
-  
+
   protected function __construct() {
     $this->roots = array(_PKGMAN_ROOT);
     $this->imported = array();
   }
-  
+
   public function add_root($root) {
     $root = realpath(rtrim($root,"\\/"));
     if (!is_dir($root)) throw new Exception("add_root($root) is not a directory!");
     if (in_array($root,$this->roots)) return;
     $this->roots[] = $root;
   }
-  
+
   public function get($package) {
     if (!isset($this->imported[$package])) return null;
     return $this->imported[$package];
   }
-  
+
   private function init_package($pkg,$prefix,$path,&$_depends) {
     $pkg_name = $pkg;
     $path = realpath($path);
@@ -41,10 +46,18 @@ class pkgman_manager {
     $depends = null;
     if (file_exists($path."/config.php")) require($path."/config.php");
     if (!is_object($package)) $package = new pkgman_package($pkg_name,$prefix,$path,$config);
-    $_depends = $depends; 
+    $_depends = $depends;
     return $package;
   }
-  
+
+  /**
+   * @desc Looks for a package directory under the defined root paths and if found performs
+   * package's initialization loading config.php (if exists) and creating a pkgman_package instance.
+   * Also, if the package states that it depends on some other packages, they will be imported
+   * automatically.
+   * @param string $pkg - package to import
+   * @param array $config - custom config passed by application
+   */
   public function import_package($pkg,$config=null) {
     if (strspn($pkg,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-") != strlen($pkg))
       throw new Exception("PKGMAN: Bad package name [$pkg]!");
@@ -67,11 +80,15 @@ class pkgman_manager {
     } else {
       $package = $this->imported[$pkg];
     }
-    // add/replace config data for the package 
+    // add/replace config data for the package
     if (is_array($config)) $package->merge_config($config);
   }
-  
-  // accepts argument list where each entry may be a package name or array of ('package_name'=>config)
+
+  /**
+   * @desc Accepts argument list where each argument can be a package name to import or array.
+   * For array arguments, each entry may be a package name or key=>value pair like this:
+   * $_pkgman->import('html',array('sql'=>$config,'utils','app'));
+   */
   public function import() {
     foreach(func_get_args() as $arg) {
       if (is_array($arg)) {
@@ -84,10 +101,14 @@ class pkgman_manager {
       }
     }
   }
-  
-  // called from __autoload()
-  // if success, return true, else false
-  // if $test_only == true, then do not parse the class's file, only check if it is present
+
+  /**
+   * @desc Called from  __autoload() to load a specific class must. Returns true on success,
+   * or false if class is not found.
+   * @param string $class_name - a class to load
+   * @param bool $test_only - if true, do not actually load class, only check if it is present
+   * @return bool
+   */
   public function load_class($class,$test_only=false) {
     foreach($this->imported as $pkg) {
       if (strpos($class,$pkg->prefix)!==0) continue;
