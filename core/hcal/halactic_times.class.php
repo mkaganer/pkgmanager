ed487d5267f26fb586550c7d2d2658d04acb8f5f
mkaganer <mkaganer@315d4af9-c016-9649-a7cf-b8f2ab84a462>
2009-12-21 16:32:55 +0000

    work on halactic times
    git-svn-id: https://svn.mehadrin-serv.com/svn/pkgman/trunk@37 315d4af9-c016-9649-a7cf-b8f2ab84a462

--- core/hcal/halactic_times.class.php
+++ core/hcal/halactic_times.class.php
@@ -14,7 +14,7 @@ class hcal_halactic_times {
     /**
      * @var float
      */
-    public $rel_hour;
+    public $rel_hr;
     /**
      * @desc Current time (set by hcal_datetime's h,m,s properties) in float hours
      * @var float
@@ -27,12 +27,40 @@ class hcal_halactic_times {
      */
     public $heb_date;
     
-    public function __construct($heb_date,$sunrise, $sunset, $rel_hour, $now) {
-        $this->heb_date = $heb_date; 
+    /**
+     * @var hcal_location
+     */
+    public $location;
+    
+    /**
+     * @desc base values for times calculations in float format
+     * @var array (float)
+     */
+    private $_base;
+    
+    /**
+     * @desc Caculated times will be stored here in string format (HH:MM)
+     * @var array
+     */
+    public $data;
+    
+    public function __construct($heb_date, $ts, $gmt_offset, $location, $now) {
+        $zenith = 90.0 + 50.0/60;
+        $this->heb_date = $heb_date;
+        $this->location = $location;
+        $this->now = $now;
+        $this->data = array();
+        
+        $lat = $location->latitude;
+        $long = $location->longitude;
+        $sunrise = date_sunrise($ts, SUNFUNCS_RET_DOUBLE, $lat, $long, $zenith, $gmt_offset);
+        $sunset = date_sunset($ts, SUNFUNCS_RET_DOUBLE, $lat, $long, $zenith, $gmt_offset);
+        
         $this->sunrise = $sunrise;
         $this->sunset = $sunset;
-        $this->rel_hour = $rel_hour;
-        $this->now = $now;
+        $this->rel_hr = ($sunset - $sunrise)/12.0;
+        
+        $this->calculate_base_times();
     }
     
     /**
@@ -51,10 +79,84 @@ class hcal_halactic_times {
      * @param float $hours "shot zmaniyuot"
      * @return string
      */
-    public function rel_hour($hours) {
-        $time = $this->sunrise+$this->rel_hour*$hours;
+    public function rel_hour($hours,$d=0.0) {
+        $time = $this->sunrise+$this->rel_hr*$hours+$d;
         return $this->format($time);
     }
+    
+    /**
+     * @return void
+     */
+    private function calculate_base_times() {
+        $this->_base = array(
+            'amud72' => ($this->sunrise - (72.0/60.0)),
+            'amud90' => ($this->sunrise - 1.5),
+            'amud120' => ($this->sunrise - 2.0),
+            'tzet18' => ($this->sunset + (18.0/60.0)),
+            'tzet24' => ($this->sunset + (24.0/60.0)),
+        );
+    }
+    
+    public function get_data() {
+        if (!empty($this->data)) return $this->data;
+        
+        // see: http://www.yeshiva.org.il/message/times.asp
+        
+        $mn = 1.0/60.0; // one absolute minute
+        $rel_hr = $this->rel_hr;
+        
+        // שעה זמנית לפי מגן אברהם
+        $rel_hr_ma = ($this->_base['tzet18'] - $this->_base['amud72']) / 12.0;
+        
+        $chatsot = ($this->sunset + $this->sunrise)/2;
+        
+        $data = array();
+        $data['sunrise'] = $this->format($this->sunrise);
+        $data['sunset'] = $this->format($this->sunset);
+        $data['amud72'] = $this->format($this->_base['amud72']+3*$mn);
+        $data['amud120'] = $this->format($this->_base['amud120']);
+        $data['tzet24'] = $this->format($this->_base['tzet24']);
+        $data['tzet18'] = $this->format($this->_base['tzet18']);
+        
+        $data['chatsot'] = $this->format($chatsot);
+        
+        // מנחה גדולה
+        // חצות + 1/2 שעה לחומרה
+        $mincha1 = $chatsot + max(0.5*$rel_hr,0.5);
+        $data['mincha1'] = $this->format($mincha1 + 3*$mn);
+        
+        // מנחה קטנה
+        $data['mincha2'] = $this->rel_hour(12.0 - 2.5,2*$mn);
+        
+        // פלג מנחה
+        $data['plag'] = $this->rel_hour(10.75,3.0*$mn);
+        
+        // משיכיר
+        // 45 דקות לפני הזריחה לחומרה
+        $misheyakir = max(($this->sunrise - (45.0/60.0)),
+            ($this->sunrise - $rel_hr*(45.0/60.0)));
+        $data['misheyakir'] = $this->format($misheyakir + 3*$mn);
+        
+        // Baal HaTaniya (שעה"ר):
+        $data['smah'] = $this->rel_hour(3.0,-3*$mn);
+        $data['tfilah'] = $this->rel_hour(4.0,-3*$mn);
+        
+        // Magen Avraham:
+        $shma_ma = $this->_base['amud72'] + 3.0*$rel_hr_ma;
+        $tfila_ma = $this->_base['amud72'] + 4.0*$rel_hr_ma;
+        $data['smah_ma'] = $this->format($shma_ma-3*$mn);
+        $data['tfilah_ma'] = $this->format($tfila_ma-3*$mn);
+        
+        // Shabbat:
+        $nerot = (float)(isset($this->location->data['nerot'])?$this->location->data['nerot']:22.0);
+        $data['sh_nerot'] = $this->format($this->sunset - $nerot*$mn);
+        $data['sh_tzet'] = $this->format($this->sunset + 40.0*$mn);
+        $data['sh_tzet_rt'] = $this->format($this->sunset + 72.0*$mn);
+        
+        $this->data = $data;
+        return $data;
+    }
+    
     
     
 }
