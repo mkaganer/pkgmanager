cbcef9e146e2ee733a343f092ee067db664d34a1
mkaganer <mkaganer@315d4af9-c016-9649-a7cf-b8f2ab84a462>
2009-12-28 12:12:50 +0000

    is_kodesh function determines if the current date/time is a "kodesh" time e.g. Shabbat or Festival (Yom Tov). Takes the "Tossefet Shabbat" 18 min before / 40 min after in account
    git-svn-id: http://svn.mehadrin-serv.com/svn/pkgman/trunk@43 315d4af9-c016-9649-a7cf-b8f2ab84a462

--- core/hcal/location_factory.class.php
+++ core/hcal/location_factory.class.php
@@ -13,13 +13,16 @@ class hcal_location_factory {
         'tel_aviv' => array('lat' => 32.05, 'long' => 34.75, 'tz'=>'Asia/Jerusalem' ),
         'bney_brak' => array('lat' => 32.0783333, 'long' => 34.8422222, 'tz'=>'Asia/Jerusalem' ),
         'beer_sheva' => array('lat' => 31.25, 'long' => 34.783333333, 'tz'=>'Asia/Jerusalem' ),
-        'eilat' => array('lat' => 29.554167, 'long' => 34.948056, 'tz'=>'Asia/Jerusalem' ),
+        'eilat' => array('lat' => 29.554167, 'long' => 34.948056, 'tz'=>'Asia/Jerusalem',
+            'chul' => false ),
+        // (see http://www.adathisraelshul.org/rabbis-study/chagim/78-passover/211-the-halachic-status-of-eilat.html)
         'beitar_elit' => array('lat' => 31.697222, 'long' => 35.1225, 'tz'=>'Asia/Jerusalem' ),
         'modiin_ilit' => array('lat' => 31.931667, 'long' => 35.043056, 'tz'=>'Asia/Jerusalem' ),
         'ashdod' => array('lat' => 31.7880556, 'long' => 34.64277778, 'tz'=>'Asia/Jerusalem' ),
         'tiberias' => array('lat' => 32.7863889, 'long' => 35.5425, 'tz'=>'Asia/Jerusalem' ),
         'haifa' => array('lat' => 32.826945, 'long' => 34.97694, 'tz'=>'Asia/Jerusalem' ),
-        'new_york' => array('lat' => 40.66909166667, 'long' => -73.94282222222, 'tz'=>'America/New_York' ),
+        'new_york' => array('lat' => 40.66909166667, 'long' => -73.94282222222, 'tz'=>'America/New_York',
+            'chul' => true ),
     );
     
     /**
